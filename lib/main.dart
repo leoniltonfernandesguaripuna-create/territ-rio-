ac1697ea8ab3b2f3d07d6d5b2c5ac319db85b531
leoniltonfernandesguaripuna-create <leoniltonfernandesguaripuna@gmail.com>
2026-09-13 20:17:46 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,4 +24,17 @@ class TerritorioApp extends StatelessWidget {
     );
   }
 }
+class Territorio {
+  final int id;
+  String nome;
+  bool isSelecionado;
+  String? imagemPath; // Armazena o caminho da foto salva no celular
+
+  Territorio({
+    required this.id,
+    required this.nome,
+    this.isSelecionado = false,
+    this.imagemPath,
+  });
+}
                     
